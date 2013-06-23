c39bd91a2fac23dc85e9db045baceb732f0375b1
Jason Fu <blackbanana2001@163.com>
2013-06-23 21:28:58 +0800

    Update apple code, include Oled(i2c) and ADXL345(SPI) verified code
    ADXL345 Device ID read out.
    OLed lighted but display random.

--- apple/SourceCode/apple.c
+++ apple/SourceCode/apple.c
@@ -2,11 +2,19 @@
 #include "twi_master.h"
 #include "nrf_gpio.h"
 #include "lerbit_oled.h"
+#include "app_util.h"
 
 #define LERBIT_OLED_RES_PIN 8
 #define LERBIT_OLED_I2C_ADDR 0x78
 #define LERBIT_OLED_W (LERBIT_OLED_I2C_ADDR)
 #define LERBIT_OLED_R (LERBIT_OLED_I2C_ADDR | TWI_READ_BIT)
+
+#define LERBIT_APPLE_PWRO_PIN_NO 1
+#define LERBIT_APPLE_LEDS_1_PIN_NO 28
+#define LERBIT_APPLE_LEDS_2_PIN_NO 29
+#define LERBIT_APPLE_LEDS_3_PIN_NO 30
+#define LERBIT_APPLE_LEDS_4_PIN_NO 0
+
 
 
 uint8_t lerbit_oled_gram[128][4]; /* 128 * 32 */
@@ -32,13 +40,13 @@ void lerbit_oled_write_data(uint8_t data)
 void lerbit_oled_refresh_gram(void)
 {
 	uint8_t i, n;
-	for(i = 0; i < 8; i++)
+	for(i = 0; i < 4; i++)
 	{
 		lerbit_oled_write_command(0xb0 + i);    //设置页地址（0~7）
 		lerbit_oled_write_command(0x00);      //设置显示位置—列低地址
 		lerbit_oled_write_command(0x10);      //设置显示位置—列高地址
 		for(n = 0; n < 128; n++)
-      lerbit_oled_write_command(lerbit_oled_gram[n][i]);
+      lerbit_oled_write_data(lerbit_oled_gram[n][i-4]);
 	}
 }
 
@@ -136,35 +144,33 @@ void lerbit_oled_showstring(uint8_t x, uint8_t y, const uint8_t *p)
 
 void lerbit_oled_init(void)
 {
-	lerbit_oled_write_command(0xAE); //关闭显示
-	lerbit_oled_write_command(0xD5); //设置时钟分频因子,震荡频率
-	lerbit_oled_write_command(80);   //[3:0],分频因子;[7:4],震荡频率
-	lerbit_oled_write_command(0xA8); //设置驱动路数
-	lerbit_oled_write_command(0x3F); //默认0x3F(1/64)
-	lerbit_oled_write_command(0xD3); //设置显示偏移
-	lerbit_oled_write_command(0x00); //默认为0
+	lerbit_oled_write_command(0xAE); 
+	lerbit_oled_write_command(0xD5); 
+	lerbit_oled_write_command(0x80); 
+	lerbit_oled_write_command(0xA8); 
+	lerbit_oled_write_command(0x1F); 
+	lerbit_oled_write_command(0xD3); 
+	lerbit_oled_write_command(0x00); 
 
-	lerbit_oled_write_command(0x40); //设置显示开始行 [5:0],行数.
+	lerbit_oled_write_command(0x40);
 
-	lerbit_oled_write_command(0x8D); //电荷泵设置
-	lerbit_oled_write_command(0x14); //bit2，开启/关闭
-	lerbit_oled_write_command(0x20); //设置内存地址模式
-	lerbit_oled_write_command(0x02); //[1:0],00，列地址模式;01，行地址模式;10,页地址模式;默认10;
-	lerbit_oled_write_command(0xA1); //段重定义设置,bit0:0,0->0;1,0->127;
-	lerbit_oled_write_command(0xC0); //设置COM扫描方向;bit3:0,普通模式;1,重定义模式 COM[N-1]->COM0;N:驱动路数
-	lerbit_oled_write_command(0xDA); //设置COM硬件引脚配置
-	lerbit_oled_write_command(0x12); //[5:4]配置
+	lerbit_oled_write_command(0x8D);
+	lerbit_oled_write_command(0x14);
+	lerbit_oled_write_command(0xA1);
+	lerbit_oled_write_command(0xC0);
+	lerbit_oled_write_command(0xDA);
+	lerbit_oled_write_command(0x02);
+	lerbit_oled_write_command(0x81);
+	lerbit_oled_write_command(0x68);
 
-	lerbit_oled_write_command(0x81); //对比度设置
-	lerbit_oled_write_command(0xEF); //1~255;默认0X7F (亮度设置,越大越亮)
-	lerbit_oled_write_command(0xD9); //设置预充电周期
-	lerbit_oled_write_command(0xf1); //[3:0],PHASE 1;[7:4],PHASE 2;
-	lerbit_oled_write_command(0xDB); //设置VCOMH 电压倍率
-	lerbit_oled_write_command(0x30); //[6:4] 000,0.65*vcc;001,0.77*vcc;011,0.83*vcc;
+	lerbit_oled_write_command(0xD9);
+	lerbit_oled_write_command(0xf1);
+	lerbit_oled_write_command(0xDB);
+	lerbit_oled_write_command(0x30);
 
-	lerbit_oled_write_command(0xA4); //全局显示开启;bit0:1,开启;0,关闭;(白屏/黑屏)
-	lerbit_oled_write_command(0xA6); //设置显示方式;bit0:1,反相显示;0,正常显示
-	lerbit_oled_write_command(0xAF); //开启显示
+	lerbit_oled_write_command(0xA4);
+	lerbit_oled_write_command(0xA6);
+	lerbit_oled_write_command(0xAF);
 	lerbit_oled_clear();
 }
 
@@ -190,18 +196,54 @@ void lerbit_acc_reg_write(uint8_t reg_addr, uint8_t reg_value)
   spi_master_tx_rx((uint32_t *)NRF_SPI0, 2, spi_tx_data, spi_rx_data);	
 }
 
+static void leds_init(void)
+{
+	GPIO_LED_CONFIG(LERBIT_APPLE_PWRO_PIN_NO); /* PWRO */
+	GPIO_LED_CONFIG(LERBIT_OLED_RES_PIN); /* OLED_RES */
+	GPIO_LED_CONFIG(LERBIT_APPLE_LEDS_1_PIN_NO);
+	GPIO_LED_CONFIG(LERBIT_APPLE_LEDS_2_PIN_NO);
+	GPIO_LED_CONFIG(LERBIT_APPLE_LEDS_3_PIN_NO);
+	GPIO_LED_CONFIG(LERBIT_APPLE_LEDS_4_PIN_NO);
+}
+
 int main(void)
 {
   uint8_t data;
+	unsigned int loop = 2000000;
 		
 	twi_master_init();
 	spi_master_init(SPI0, SPI_MODE3, 0);
+	
+	leds_init();
+	nrf_gpio_pin_set(LERBIT_APPLE_PWRO_PIN_NO); /* set PWRO */
+	
+	/* light leds */
+	nrf_gpio_pin_set(LERBIT_APPLE_LEDS_1_PIN_NO);
+	nrf_gpio_pin_set(LERBIT_APPLE_LEDS_2_PIN_NO);
+	nrf_gpio_pin_set(LERBIT_APPLE_LEDS_3_PIN_NO);
+	nrf_gpio_pin_set(LERBIT_APPLE_LEDS_4_PIN_NO);
+	
+	while(loop--) {}
+		
+	nrf_gpio_pin_clear(LERBIT_APPLE_LEDS_1_PIN_NO);
+	nrf_gpio_pin_clear(LERBIT_APPLE_LEDS_2_PIN_NO);
+	nrf_gpio_pin_clear(LERBIT_APPLE_LEDS_3_PIN_NO);
+	nrf_gpio_pin_clear(LERBIT_APPLE_LEDS_4_PIN_NO);
 
+#if 1
+	nrf_gpio_pin_set(LERBIT_OLED_RES_PIN);
+	loop = 160; /* 10us */
+	while(loop--) {}
+	nrf_gpio_pin_clear(LERBIT_OLED_RES_PIN);
+	loop = 160; /* 10us */
+	while(loop--) {}
+	nrf_gpio_pin_set(LERBIT_OLED_RES_PIN);
   lerbit_oled_init();
 
   lerbit_oled_showstring(12, 11, (const uint8_t *)"Lerbit Studio");
   lerbit_oled_showstring(16, 37, (const uint8_t *)"Welcome You!");
   lerbit_oled_refresh_gram(); 
+#endif
 
   data = 0x00;
   lerbit_acc_reg_write(0x31, data);
